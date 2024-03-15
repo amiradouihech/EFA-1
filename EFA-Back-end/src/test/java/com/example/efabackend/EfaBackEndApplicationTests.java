737d6a0a2aefa2cd7cd60838ea1bd7135158dd5d
kitokid <ourabisaif051@gmail.com>
2024-03-16 00:42:08 +0100

    int bacnend

--- EFA-Back-end/src/test/java/com/example/efabackend/EfaBackEndApplicationTests.java
+++ EFA-Back-end/src/test/java/com/example/efabackend/EfaBackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.efa;
+package com.example.efabackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EfaApplicationTests {
+class EfaBackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
