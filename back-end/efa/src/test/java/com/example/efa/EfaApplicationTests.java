e48eda0dfdd5ce4585ecec6a1f4b1623a453a6a1
kitokid <ourabisaif48@gmail.com>
2024-03-15 08:59:20 +0000

    first commit

---
+++ back-end/efa/src/test/java/com/example/efa/EfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
